3a3263517610f2bae59ca4876ed82f8e36101d18
KodaiTakao <takaokodai@team-lab.com>
2017-06-27 23:08:05 +0900

    Added Ribbon Audio Visualizer

---
+++ Assets/AudioVisual/Manager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kodai.Audio.Ribbon {
+
+    public class Manager : MonoBehaviour {
+
+        public static AudioSource audio;
+        public static int ribbonNum = 50;
+        public static int band = 1024;
+
+        public GameObject ribbonPrefab;
+
+        Ribbon[] ribbons;
+
+        public static float[][] audioData;
+
+        void Start() {
+            audio = GetComponent<AudioSource>();
+
+            ribbons = new Ribbon[ribbonNum];
+            for (int i = 0; i < ribbons.Length; i++) {
+                GameObject o = Instantiate(ribbonPrefab, new Vector3(0, 0, i*2), Quaternion.identity, transform);
+                ribbons[i] = o.GetComponent<Ribbon>();
+                ribbons[i].id = i;
+
+                o.GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(0, 0.8f - (float)(i+1)/ribbonNum, 1 - (float)(i + 1) / ribbonNum));
+            }
+
+            audioData = new float[100][];
+            for (int i = 0; i < 100; i++) {
+                audioData[i] = new float[band];
+                for (int j = 0; j < band; j++) {
+                    audioData[i][j] = 0;
+                }
+            }
+
+        }
+
+        int frame = 0;
+        int count = 0;
+        void Update() {
+
+            audio.GetOutputData(audioData[0], 1);   // バンド数変えると変更が必要
+
+            // 手前のデータをコピー
+            // 昇順じゃダメ
+            for (int i = count; i > 0; i--) {
+                audioData[i - 1].CopyTo(audioData[i], 0);   // ミソ
+            }
+
+            frame++;
+
+            count = frame / 20;
+            if (count > ribbonNum) count = ribbonNum;
+        }
+    }
+}
---
+++ Assets/AudioVisual/Ribbon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Kodai.Audio.Ribbon {
+
+    [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(MeshRenderer))]
+    public class Ribbon : MonoBehaviour {
+
+        public int id;
+
+        public Material mat;
+
+        Mesh mesh;
+
+        void Start() {
+            MeshFilter mf = GetComponent<MeshFilter>();
+            mesh = new Mesh();
+            mf.mesh = mesh;
+        }
+
+        
+        void Update() {
+
+            Vector3[] v = new Vector3[1024 * 2];
+            for(int i = 0; i<1024; i+=2) {
+                v[i] = new Vector3(0.2f * i, Manager.audioData[id][i] * 30 + 0.3f, transform.position.z);
+                v[i + 1] = new Vector3(0.2f * i, Manager.audioData[id][i] * 30 - 0.3f, transform.position.z);
+            }
+            mesh.vertices = v;
+
+            int[] idx = new int[1024 * 2 * 3];
+            int count = 0;
+            for(int i = 0; i<1023; i++) {
+                idx[count] = 2 * i;
+                idx[count + 1] = 2 * i + 3;
+                idx[count + 2] = 2 * i + 1;
+                idx[count + 3] = 2 * i;
+                idx[count + 4] = 2 * i + 2;
+                idx[count + 5] = 2 * i + 3;
+                count += 6;
+            }
+            mesh.SetIndices(idx, MeshTopology.Triangles, 0);
+            mesh.RecalculateNormals();
+
+        }
+        
+    }
+}
